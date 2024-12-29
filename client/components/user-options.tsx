def26de0fcfb92a332c8c6bea43abc6e78f94666
Miroslav Hanisko <miroslav.hanisko@student.tuke.sk>
2024-12-29 17:04:18 +0100

    customize user options

--- client/components/user-options.tsx
+++ client/components/user-options.tsx
@@ -10,6 +10,9 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Button } from "./ui/button"
+import { LogOut, Settings, User } from "lucide-react"
+import { Separator } from "./ui/separator"
 
 export function UserOptions() {
   const { setTheme } = useTheme()
@@ -26,11 +29,34 @@ export function UserOptions() {
         </Avatar>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
+        <div className="flex items-center justify-between px-4 py-2 gap-5">
+          <Avatar>
+            <AvatarImage
+              src="teacher_profile_picture.jpg"
+              alt="Teacher profile picture"
+            />
+            <AvatarFallback>MBi</AvatarFallback>
+          </Avatar>
+          <div className="flex flex-col items-end">
+            <span className="text-sm font-semibold">Miroslav Hanisko</span>
+            <span className="text-xs text-gray-500">miroslav.hanisko@student.tuke.sk</span>
+          </div>
+        </div>
         <DropdownMenuItem >
-          Profile
+          <Button className="w-full">
+            <User /> Profile
+          </Button>
         </DropdownMenuItem>
         <DropdownMenuItem >
-          Logout
+          <Button className="w-full">
+            <Settings /> Settings
+          </Button>
+        </DropdownMenuItem>
+        <Separator />
+        <DropdownMenuItem >
+          <Button className="w-full">
+            <LogOut /> Logout
+          </Button>
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
