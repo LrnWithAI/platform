5c2838ece1b12d1be4c123277d6970f62c7e926d
Miroslav Hanisko <miroslav.hanisko@student.tuke.sk>
2024-12-31 13:31:11 +0100

    update login & register UI

--- client/app/register/page.tsx
+++ client/app/register/page.tsx
@@ -1,6 +1,6 @@
 import { GalleryVerticalEnd } from "lucide-react"
 
-import { LoginForm } from "@/components/login-form"
+import { RegisterForm } from "@/components/register-form"
 
 export default function LoginPage() {
   return (
@@ -12,7 +12,7 @@ export default function LoginPage() {
           </div>
           LrnWithAI
         </a>
-        <LoginForm />
+        <RegisterForm />
       </div>
     </div>
   )
