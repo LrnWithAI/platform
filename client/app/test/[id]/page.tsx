d0b3977d7e5130a925be7b5f1ddff9430de56ed7
Matej Bendik <matej.bendik@student.tuke.sk>
2025-03-13 12:57:58 +0100

    Add test page with submit feature and checking right answers

--- client/app/test/[id]/page.tsx
+++ client/app/test/[id]/page.tsx
@@ -1,20 +1,32 @@
 "use client";
 
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 import { toast } from "react-toastify";
 import { getTestById } from "@/actions/testActions";
 import { Test } from "@/types/test";
+import { useUserStore } from "@/stores/userStore";
+import { useForm, Controller } from "react-hook-form";
 
 import { Label } from "@/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 
 const TestPage = () => {
+  const router = useRouter();
   const params = useParams<{ id: string }>();
+  const user = useUserStore((state) => state.user);
   const testId = Number(params.id);
   const [test, setTest] = useState<Test | null>(null);
+
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   async function fetchTestById() {
     try {
@@ -38,9 +50,21 @@ const TestPage = () => {
     console.log("test data", test);
   }, [test]);
 
+  const onSubmit = async (data: any) => {
+    const queryParams = new URLSearchParams({
+      answers: JSON.stringify(data.answers),
+    });
+
+    router.push(`/test/${test?.id}/submit?${queryParams.toString()}`);
+    console.log("test check data", data);
+  };
+
   return (
     <div className="mx-4 mb-20 mt-0 md:mt-6">
-      <form className="flex flex-col justify-center max-w-4xl mx-auto">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col justify-center max-w-4xl mx-auto"
+      >
         <div className="flex flex-row justify-between items-center my-8">
           <div>
             <h2 className="text-lg md:text-2xl font-bold">{test?.title}</h2>
@@ -60,7 +84,10 @@ const TestPage = () => {
                   )
                 : "Invalid date"}
             </h4>
-            <p>by matejkooo</p>
+            <p>
+              by{" "}
+              {user?.id === test?.created_by ? user?.username : "some nickname"}
+            </p>
           </div>
         </div>
 
@@ -70,19 +97,49 @@ const TestPage = () => {
             key={index}
             className="dark:bg-muted border flex flex-col rounded-md p-6 space-y-4 bg-gray-100 my-2 md:my-4"
           >
-            <p>{question.question}</p>
+            <p>
+              {index + 1}. {question.question}
+            </p>
             <div className="py-2">
-              <RadioGroup className="space-y-2">
-                {question.answers.map((answer, index) => (
-                  <div key={index} className="flex items-center space-x-2">
-                    <RadioGroupItem value={answer} />
-                    <Label htmlFor={answer}>{answer}</Label>
-                  </div>
-                ))}
-              </RadioGroup>
+              <Controller
+                name={`answers.${index + 1}`}
+                control={control}
+                rules={{ required: "This question is required" }}
+                render={({ field }) => (
+                  <RadioGroup
+                    className="space-y-2"
+                    onValueChange={(selectedValue) =>
+                      field.onChange(Number(selectedValue))
+                    }
+                    value={field.value?.toString() || ""}
+                  >
+                    {question.answers.map((answer, ansIndex) => (
+                      <div
+                        key={ansIndex}
+                        className="flex items-center space-x-2"
+                      >
+                        <RadioGroupItem
+                          value={ansIndex.toString()}
+                          id={`answer-${index + 1}-${ansIndex}`}
+                        />
+                        <Label htmlFor={`answer-${index + 1}-${ansIndex}`}>
+                          {answer}
+                        </Label>
+                      </div>
+                    ))}
+                  </RadioGroup>
+                )}
+              />
             </div>
           </div>
         ))}
+
+        {/* Submit Button */}
+        <div className="flex justify-center">
+          <Button type="submit" className="bg-purple">
+            Submit
+          </Button>
+        </div>
       </form>
     </div>
   );
