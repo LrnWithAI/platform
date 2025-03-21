9d6cc88ad4d920412d083b6e86063fccbfd14361
Matej Bendik <matej.bendik@student.tuke.sk>
2025-03-21 13:33:06 +0100

    Conditionally render content based on option from url

--- client/app/test/create/page.tsx
+++ client/app/test/create/page.tsx
@@ -112,157 +112,165 @@ export default function CreateTest() {
         </h2>
       </div>
 
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="flex flex-col justify-center max-w-4xl mx-auto"
-      >
-        <div className="dark:bg-muted border rounded-md p-6 space-y-4 bg-gray-100">
-          <div className="space-y-2 pb-2">
-            <Label htmlFor="title">Title</Label>
-            <Input
-              id="title"
-              type="text"
-              placeholder="Enter a title, like Introduction to AI"
-              {...register("title")}
-            />
-            {errors.title && (
-              <p className="text-sm text-red-500">{errors.title.message}</p>
-            )}
-          </div>
-
-          <div className="space-y-2">
-            <Label htmlFor="description">Description</Label>
-            <Textarea
-              placeholder="Add a description..."
-              {...register("description")}
-            />
-            {errors.description && (
-              <p className="text-sm text-red-500">
-                {errors.description.message}
-              </p>
-            )}
-          </div>
-
-          <div className="flex flex-row">
-            <div className="flex items-center gap-3 w-1/2">
-              <Label htmlFor="title">Visible to</Label>
-              <Select onValueChange={(value) => setValue("visibility", value)}>
-                <SelectTrigger className="w-[180px]">
-                  <SelectValue placeholder="Everyone" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectGroup>
-                    <SelectItem value="everyone">Everyone</SelectItem>
-                    <SelectItem value="users-with-password">
-                      Users with password
-                    </SelectItem>
-                    <SelectItem value="just-me">Just me</SelectItem>
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
-            </div>
-            <div className="flex items-center w-1/2">
-              <Label htmlFor="category">Category</Label>
+      {option === "manually" ? (
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col justify-center max-w-4xl mx-auto"
+        >
+          <div className="dark:bg-muted border rounded-md p-6 space-y-4 bg-gray-100">
+            <div className="space-y-2 pb-2">
+              <Label htmlFor="title">Title</Label>
               <Input
-                id="category"
+                id="title"
                 type="text"
-                placeholder="AI"
-                {...register("category")}
+                placeholder="Enter a title, like Introduction to AI"
+                {...register("title")}
               />
-              {errors.category && (
+              {errors.title && (
+                <p className="text-sm text-red-500">{errors.title.message}</p>
+              )}
+            </div>
+
+            <div className="space-y-2">
+              <Label htmlFor="description">Description</Label>
+              <Textarea
+                placeholder="Add a description..."
+                {...register("description")}
+              />
+              {errors.description && (
                 <p className="text-sm text-red-500">
-                  {errors.category.message}
+                  {errors.description.message}
                 </p>
               )}
             </div>
-          </div>
-        </div>
 
-        <div className="my-4">
-          <h2 className="text-xl font-bold">Questions</h2>
-        </div>
-
-        <div className="space-y-6">
-          {fields.map((field, questionIndex) => (
-            <div
-              key={field.id}
-              className="flex flex-col dark:bg-muted border rounded-md p-6 space-y-2"
-            >
-              <div>
-                <div className="mb-2 space-y-2">
-                  <Label>Question {questionIndex + 1}</Label>
-                  <Input
-                    type="text"
-                    placeholder="Enter a question, like What is AI?"
-                    {...register(`questions.${questionIndex}.question`)}
-                  />
-                </div>
-
-                <div className="mb-2 space-y-2">
-                  <Label>Answers</Label>
-                  {Array(4)
-                    .fill(null)
-                    .map((_, answerIndex) => {
-                      return (
-                        <div
-                          key={answerIndex}
-                          className="flex items-center space-x-2"
-                        >
-                          <Input
-                            type="text"
-                            placeholder={`Answer ${String.fromCharCode(
-                              65 + answerIndex
-                            )}`}
-                            {...register(
-                              `questions.${questionIndex}.answers.${answerIndex}`
-                            )}
-                          />
-                          <Button
-                            variant={
-                              selectedCorrectAnswers[questionIndex] ===
-                              answerIndex
-                                ? "default"
-                                : "outline"
-                            }
-                            size="icon"
-                            onClick={() =>
-                              setCorrectAnswer(questionIndex, answerIndex)
-                            }
-                            type="button"
-                          >
-                            <Check />
-                          </Button>
-                        </div>
-                      );
-                    })}
-                </div>
+            <div className="flex flex-row">
+              <div className="flex items-center gap-3 w-1/2">
+                <Label htmlFor="title">Visible to</Label>
+                <Select
+                  onValueChange={(value) => setValue("visibility", value)}
+                >
+                  <SelectTrigger className="w-[180px]">
+                    <SelectValue placeholder="Everyone" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectItem value="everyone">Everyone</SelectItem>
+                      <SelectItem value="users-with-password">
+                        Users with password
+                      </SelectItem>
+                      <SelectItem value="just-me">Just me</SelectItem>
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
               </div>
-
-              <Button
-                variant="destructive"
-                className="bg-red-500 w-36 self-end"
-                onClick={() => removeQuestion(questionIndex)}
-              >
-                Remove question
-              </Button>
+              <div className="flex items-center w-1/2">
+                <Label htmlFor="category">Category</Label>
+                <Input
+                  id="category"
+                  type="text"
+                  placeholder="AI"
+                  {...register("category")}
+                />
+                {errors.category && (
+                  <p className="text-sm text-red-500">
+                    {errors.category.message}
+                  </p>
+                )}
+              </div>
             </div>
-          ))}
-        </div>
+          </div>
 
-        {/* Add New Question */}
-        <div className="flex justify-center my-6">
-          <Button variant="outline" onClick={addQuestion}>
-            Add Question
-          </Button>
-        </div>
+          <div className="my-4">
+            <h2 className="text-xl font-bold">Questions</h2>
+          </div>
 
-        {/* Submit Button */}
-        <div className="flex justify-center">
-          <Button type="submit" className="bg-purple hover:bg-purple-500">
-            Create Test
-          </Button>
-        </div>
-      </form>
+          <div className="space-y-6">
+            {fields.map((field, questionIndex) => (
+              <div
+                key={field.id}
+                className="flex flex-col dark:bg-muted border rounded-md p-6 space-y-2"
+              >
+                <div>
+                  <div className="mb-2 space-y-2">
+                    <Label>Question {questionIndex + 1}</Label>
+                    <Input
+                      type="text"
+                      placeholder="Enter a question, like What is AI?"
+                      {...register(`questions.${questionIndex}.question`)}
+                    />
+                  </div>
+
+                  <div className="mb-2 space-y-2">
+                    <Label>Answers</Label>
+                    {Array(4)
+                      .fill(null)
+                      .map((_, answerIndex) => {
+                        return (
+                          <div
+                            key={answerIndex}
+                            className="flex items-center space-x-2"
+                          >
+                            <Input
+                              type="text"
+                              placeholder={`Answer ${String.fromCharCode(
+                                65 + answerIndex
+                              )}`}
+                              {...register(
+                                `questions.${questionIndex}.answers.${answerIndex}`
+                              )}
+                            />
+                            <Button
+                              variant={
+                                selectedCorrectAnswers[questionIndex] ===
+                                answerIndex
+                                  ? "default"
+                                  : "outline"
+                              }
+                              size="icon"
+                              onClick={() =>
+                                setCorrectAnswer(questionIndex, answerIndex)
+                              }
+                              type="button"
+                            >
+                              <Check />
+                            </Button>
+                          </div>
+                        );
+                      })}
+                  </div>
+                </div>
+
+                <Button
+                  variant="destructive"
+                  className="bg-red-500 w-36 self-end"
+                  onClick={() => removeQuestion(questionIndex)}
+                >
+                  Remove question
+                </Button>
+              </div>
+            ))}
+          </div>
+
+          {/* Add New Question */}
+          <div className="flex justify-center my-6">
+            <Button variant="outline" onClick={addQuestion}>
+              Add Question
+            </Button>
+          </div>
+
+          {/* Submit Button */}
+          <div className="flex justify-center">
+            <Button type="submit" className="bg-purple hover:bg-purple-500">
+              Create Test
+            </Button>
+          </div>
+        </form>
+      ) : option === "withAI" ? (
+        <p>Upload file here </p>
+      ) : (
+        <p>Bad option provided</p>
+      )}
     </div>
   );
 }
